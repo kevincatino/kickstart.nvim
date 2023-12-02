1d379ae1da43d38d159b02210fed069edc5da0cd
khcatino <kcatino@itba.edu.ar>
2023-12-02 10:54:00 -0300

    Unify nvim options in set.lua file

--- lua/khcatino/set.lua
+++ lua/khcatino/set.lua
@@ -15,8 +15,6 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -25,82 +23,43 @@ vim.opt.colorcolumn = "120"
 
 vim.opt.scrolloff = 8
 
-vim.opt.updatetime = 50
 
 
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
 
+-- Set highlight on search
+vim.o.hlsearch = false
 
+-- Set up autocmd to switch line numbers based on mode
+vim.api.nvim_exec([[
+  augroup NumberToggle
+    autocmd!
+    autocmd InsertEnter * set norelativenumber
+    autocmd InsertLeave * set relativenumber
+  augroup END
+]], false)
 
+-- Set the initial configuration
+vim.o.relativenumber = true  -- Show relative line numbers initially
+vim.o.number = true      -- Show absolute line numbers initially
 
+-- Enable mouse mode
+vim.o.mouse = 'a'
 
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
 
+-- Decrease update time
+vim.o.updatetime = 50
+vim.o.timeoutlen = 300
 
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
