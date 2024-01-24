67e4b0da30c603bfa85f74dee56f4882f4148379
kevincatino <kcatino@itba.edu.ar>
2024-01-24 14:22:39 -0300

    Conditionally load custom clipboard according to os

--- init.lua
+++ init.lua
@@ -1,15 +1,18 @@
--- vim.g.clipboard = {
---      name = 'WslClipboard',
---      copy = {
---         ["+"] = 'clip.exe',
---         ["*"] = 'clip.exe',
---       },
---      paste = {
---         ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---         ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---      },
---      cache_enabled = 0,
---    }
+IS_WINDOWS = vim.fn.has('windows')
+if IS_WINDOWS then
+vim.g.clipboard = {
+     name = 'WslClipboard',
+     copy = {
+        ["+"] = 'clip.exe',
+        ["*"] = 'clip.exe',
+      },
+     paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+     },
+     cache_enabled = 0,
+   }
+end
 --[[
 
 =====================================================================
